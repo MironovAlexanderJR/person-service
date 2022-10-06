b6c69d946a30582f99686bdbab0b9a60ecbf6404
Miron <alexander.mironov.jr@ya.ru>
2022-10-06 17:00:59 +0300

    initial structure

---
+++ core/src/test/java/liga/medical/medicalpersonservice/core/MedicalPersonServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package liga.medical.medicalpersonservice.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MedicalPersonServiceApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
